adfd7cbc49774f164b8f190b9ea6f1cbacf394b6
Keti1501 <katjarost1501@gmail.com>
2023-10-30 21:30:29 +0200

    corrected

--- DZ 5 Array/index.js
+++ DZ 5 Array/index.js
@@ -1,4 +1,4 @@
-// Confirms
+// // Confirms
 
 const answers = [
     confirm("Питання 1: Вам подобається ця гра?"),
@@ -8,7 +8,7 @@ const answers = [
 
 alert(answers);
 
-// Prompts
+// // Prompts
 
 const questions = [
     "Яке ваше ім'я?",
@@ -22,9 +22,9 @@ answer[0] = prompt(questions[0]);
 answer[1] = prompt(questions[1]);
 answer[2] = prompt(questions[2]);
 
-alert(answer);
+// alert(answer);
 
-// Item access
+// // Item access
 
 const userInput = prompt("Введіть індекс у масиві:");
 const myArray = [10, 20, 30, 40, 50];
@@ -38,7 +38,7 @@ let arr = [];
 arr[userInput1] =userInput2;
 alert(arr);
 
-// Multiply table
+// // Multiply table
 
 const multiplicatTab = [
     [0,0,0,0,0],
@@ -50,7 +50,7 @@ const multiplicatTab = [
 
   console.log(multiplicatTab[3][3]);
 
-//   Multiply table slice
+// //   Multiply table slice
 
 const multiplicationTable1 = [
     [0, 0, 0, 0, 0],
@@ -65,7 +65,7 @@ for( let i = 0; i <= array.length-1; i++){
 }
 console.log(array);
 
-// IndexOf Word
+// // IndexOf Word
 
 let usInp = prompt("Введіть рядок із кількох слів:");
 let word1 = prompt("Введіть потрібне слово:");
@@ -74,7 +74,7 @@ let res = arr.indexOf(word1);
 
 alert(res);
 
-// Reverse
+// // Reverse
 
 const userInputArr = [];
 
@@ -89,7 +89,7 @@ for (let i = 1; i <= 5; i++) {
 }
 console.log(reversedArr);
 
-// Reverse 2
+// // Reverse 2
 
 const revArr = [];
 for (let i = 1; i <= 5; i++) {
@@ -99,7 +99,7 @@ for (let i = 1; i <= 5; i++) {
 
 console.log(revArr);
 
-// Copy
+// // Copy
 
 const multiplicationTable5 = [
     [0,0,0,0,0],
@@ -109,15 +109,15 @@ const multiplicationTable5 = [
     [0,4,8,12,16]
 ];
 
-const multiplicationTable2 = multiplicationTable5
+const multiplicationTable2 = multiplicationTable5.slice(0);
 console.log(multiplicationTable2);
 
 // Deep Copy
 
-const multiplicationTable3 = multiplicationTable5.slice();
+const multiplicationTable3 = JSON.parse(JSON.stringify(multiplicationTable5));
 console.log(multiplicationTable3);
 
-// Array Equals
+// // Array Equals
 
 let arr = [1,2,3];
 let arr2 = arr;
@@ -126,16 +126,25 @@ console.log(arr2);
 
 // Flat
 
-const multiplicationTable4 = [
-    [...multiplicationTable[0]],
-    [...multiplicationTable[1]],
-    [...multiplicationTable[2]],
-    [...multiplicationTable[3]],
-    [...multiplicationTable[4]]
+const multiplicatTab = [
+    [0,0,0,0,0],
+    [0,1,2,3,4],
+    [0,2,4,6,8],
+    [0,3,6,9,12],
+    [0,4,8,12,16]
 ];
+
+const multiplicationTable4 = [];
+
+for (let i = 0; i < 5; i++) {
+  for (let j = 0; j < 5; j++) {
+    multiplicationTable4.push(multiplicatTab[i][j]);
+  }
+}
+
 console.log(multiplicationTable4);
 
-// Destruct
+// // Destruct
 
 const inputStrings = prompt("Введіть рядок:");
 const [firstChar, , , , fifthChar, , , , ninthChar] = inputStrings;
@@ -144,7 +153,7 @@ console.log(firstChar);
 console.log(fifthChar);
 console.log(ninthChar);
 
-// Destruct default
+// // Destruct default
 
 const inputStr = prompt("Введіть рядок:");
 const [secondChar = "!", , fourthChar = "!", fifthChar1 = "!"] = inputStr;
@@ -153,7 +162,7 @@ console.log(secondChar);
 console.log(fourthChar);
 console.log(fifthChar1);
 
-// Multiply table rest
+// // Multiply table rest
 
 const multiplicationTab = [
     [0, 0, 0, 0, 0],
@@ -164,18 +173,19 @@ const multiplicationTab = [
 ];
 
 const [ [...rest1], [...rest2], [...rest3], [...rest4]] = multiplicationTab;
-const resArray = [rest1, rest2, rest3, rest4].flat(1).filter(element => element !== 0);;
+const resArray = [...rest1, ...rest2, ...rest3, ...rest4].filter(element => element !== 0);
+
 
 console.log(resArray);
 
-// For Alert
+// // For Alert
 
 const names1 =  ["John", "Paul", "George", "Ringo"];
 for (const name of names1) {
      alert(`Hello, ${name}`)
 }
 
-// For Select Option
+// // For Select Option
 
 const currencies1 = ["USD", "EUR", "GBP", "UAH"]
 let   str1 = "<select>"
@@ -185,7 +195,7 @@ for (const currency of currencies1){
 str1 += "</select>"
 document.write(str1);
 
-// For Table Horizontal
+// // For Table Horizontal
 
 const names2 = ["John", "Paul", "George", "Ringo"]
 let   str2 = "<table><tr>"
@@ -195,7 +205,7 @@ for (const name of names2){
 str2 += "</tr></table>"
 document.write(str2);
 
-// For Table Vertical
+// // For Table Vertical
 
 const names = ["John", "Paul", "George", "Ringo"];
 let string = "<table>";
@@ -207,7 +217,7 @@ for (const name of names) {
 string += "</table>";
 document.write(string);
 
-// For Table Letters
+// // For Table Letters
 
 const currencies = ["USD", "EUR", "GBP", "UAH"];
 let   str5 = "<table style=' border-collapse: collapse'>";
@@ -230,7 +240,7 @@ for (const currency of currencies){
 str5 += "</table>";
 document.write(str5);
 
-// For Multiply Table
+// // For Multiply Table
 
 const multiplicationTable = [
     [0, 0, 0, 0, 0],
@@ -257,7 +267,7 @@ for (const [i, indexNum] of multiplicationTable.entries()){
 str+= "</table>";
 document.write(str);
 
-// Function Capitalize
+// // Function Capitalize
 
 const capitalize = str => {
     let result = str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
@@ -266,7 +276,7 @@ const capitalize = str => {
   
   console.log(capitalize("cANBerRa")); 
   
-// //   Map Capitalize
+// // //   Map Capitalize
 
 const inputString = prompt("Введіть рядок:");
 const words = inputString.split(" "); 
@@ -276,7 +286,7 @@ const resultString = capitalizedWords.join(" ");
 
 console.log(resultString);
 
-// Filter Lexics
+// // Filter Lexics
 
 const user = prompt("Введіть рядок:");
 const words1 = user.split(" ");
@@ -291,7 +301,7 @@ const resString = filteredWords.join(" ");
 
 console.log(resString); 
 
-// Beep Lexics
+// // Beep Lexics
 
 const input = prompt("Введіть рядок:");
 const word = input.split(" ");
@@ -307,7 +317,7 @@ const resultStr = censoredWords.join(" ");
 console.log(resultStr); 
 
 
-// Reduce HTML
+// // Reduce HTML
 
 const currencie = ["USD", "EUR", "GBP", "UAH"];
 const selectString = currencie.reduce((accumulator, currency) => {
@@ -316,7 +326,7 @@ const selectString = currencie.reduce((accumulator, currency) => {
 
 document.write(selectString); 
 
-// For Brackets Hell Check
+// // For Brackets Hell Check
 
 const line = prompt();
 const bracketsStack = [];
